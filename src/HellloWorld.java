3d0c68f6435be56af18ee9df0637f8465c34c05a
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-09-16 16:10:43 -0500

    finish syntax exercise

---
+++ src/HellloWorld.java
@@ -0,0 +1,26 @@
+public class HellloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string, there are many like it but this one is mine";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+        System.out.println(y);
+
+
+
+    }
+
+}
