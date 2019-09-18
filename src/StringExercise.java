90cdc96ace0c330196e3e6909702696a8c64ac32
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-09-18 12:14:13 -0500

    finish part 1

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,6 +1,6 @@
 public class StringExercise {
     public static void main(String[] args) {
-        String message = "I can do backslashes ";
+        String message = "I can do backslashes\\, double backslashes \\\\, \nand the amazing triple backslash \\\\\\! ";
 
         System.out.println(message);
     }
