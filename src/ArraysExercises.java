4c7be7ad74c9e8de77581ce4916fdc7c91851886
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-09-25 15:51:25 -0500

    fixed arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,25 +2,39 @@ import java.sql.Array;
 import java.util.Arrays;
 
 public class ArraysExercises {
-    static Person[] person = new Person[3];
-    static Person[] addPerson(Person[] arr){
-        Person[] newArr = Arrays.copyOf(arr, arr.length+1);
+    static Person[] people = new Person[3];
+    static Person[] addPerson(Person[] arr, Person name) {
+        int index = arr.length;
+        Person[] newArr = Arrays.copyOf(arr, arr.length + 1);
+
+        newArr[index] = name;
         return newArr;
     }
+
+
 
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString( numbers));
-        person[0] = new Person("Matt Greco");
-        person[1] = new Person("Mark Munter");
-        person[2] = new Person("Bash Anglo");
+        people[0] = new Person("Matt Greco");
+        people[1] = new Person("Mark Munter");
+        people[2] = new Person("Bash Anglo");
 
-        for (Person name : person){
-            System.out.println(name.getName());
+        for (Person person : people){
+            System.out.println(person.getName());
+
         }
 
-        int test = addPerson(person).length;
-        System.out.println(test);
+        Person[] newPeople = addPerson(people, new Person("izzy Brash"));
+
+        System.out.println(newPeople[3].getName());
+
+
+
+
+
+
+
 
 
 
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,12 @@
+package movies;
+import util.Input;
+
+
+public class MoviesApplication {
+
+    public static void main(String[] args) {
+//        Input input = new Input();
+//        Movie[] movies = Movie;
+
+    }
+}
