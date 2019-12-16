39ca7e2135f1461e78ace7fa8cea4322b0783370
Isaiah Brashears <izzyb@Isaiahs-MacBook-Pro.local>
2019-12-16 15:57:24 -0600

    .

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,5 +1,13 @@
 package shapes;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rectangle extends Quadrilateral implements Measurable {
     @Override
     public double getPerimeter() {
@@ -33,5 +41,18 @@ public class Rectangle extends Quadrilateral implements Measurable {
     @Override
     public void setLength(double l) {
         super.setLength(l);
+    }
+
+    public static void main(String[] args) {
+        String x = "1";
+
+        System.out.println();;
+
+        List<Integer> list = new ArrayList<>();
+
+        int coins[] = {1,2,3};
+
+        System.out.println(coins);
+
     }
 }
